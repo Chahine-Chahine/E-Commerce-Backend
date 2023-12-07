bfecd24cc071ce7f65cc3c7a5a0758412f8a7a13
Chahine-Chahine <chahinesquare@hotmail.com>
2023-12-07 15:36:13 +0200

    implemented update for sellers in products table

--- Backend/API/Products/update.php
+++ Backend/API/Products/update.php
@@ -1,41 +1,75 @@
 <?php
-header('Access-Control-Allow-Origin:*');
+header('Access-Control-Allow-Origin: *');
 include("../../connection.php");
+require '../../../vendor/autoload.php';
 
-$product_id = $_POST['product_id'];
-$product_name = $_POST['product_name'];
-$description = $_POST['description'];
-$price = $_POST['price'];
-$seller_id = $_POST['seller_id'];
+use Firebase\JWT\JWT;
+use Firebase\JWT\ExpiredException;
+use Firebase\JWT\Key;
 
-$query = $mysqli->prepare('
-    UPDATE products
-    SET
-        product_name = ?,
-        description = ?,
-        price = ?,
-        seller_id = ?
-    WHERE
-        product_id = ?
-');
-
-$query->bind_param('ssdsi', $product_name, $description, $price, $seller_id, $product_id);
-$query->execute();
-
-$response = [];
-
-if ($mysqli->affected_rows > 0) {
-    $response['status'] = 'Success';
-    $response['message'] = 'Product updated successfully';
-} else {
-    $response['status'] = 'Error';
-    $response['message'] = 'Product not found or no changes made';
+$headers = getallheaders();
+if (!isset($headers['Authorization']) || empty($headers['Authorization'])) {
+    http_response_code(401);
+    echo json_encode(["error" => "unauthorized"]);
+    exit();
 }
 
-echo json_encode($response);
+$authorizationHeader = $headers['Authorization'];
+$token = null;
 
-// Close the statement
-$query->close();
-// Close the database connection
-$mysqli->close();
+$token = trim(str_replace("Bearer", '', $authorizationHeader));
+
+if (!$token) {
+    http_response_code(401);
+    echo json_encode(["error" => "unauthorized"]);
+    exit();
+}
+
+try {
+    $key = "your_secret"; 
+    $decoded = JWT::decode($token, new Key($key, 'HS256'));
+
+    $product_id = $_POST['product_id'];
+    $product_name = $_POST['product_name'];
+    $description = $_POST['description'];
+    $price = $_POST['price'];
+    $seller_id = $_POST['seller_id'];
+
+    $query = $mysqli->prepare('
+        UPDATE products
+        SET
+            product_name = ?,
+            description = ?,
+            price = ?,
+            seller_id = ?
+        WHERE
+            product_id = ?
+    ');
+
+    $query->bind_param('ssdsi', $product_name, $description, $price, $seller_id, $product_id);
+    $query->execute();
+
+    $response = [];
+
+    if ($mysqli->affected_rows > 0) {
+        $response['status'] = 'Success';
+        $response['message'] = 'Product updated successfully';
+    } else {
+        $response['status'] = 'Error';
+        $response['message'] = 'Product not found or no changes made';
+    }
+
+    echo json_encode($response);
+
+    // Close the statement
+    $query->close();
+    // Close the database connection
+    $mysqli->close();
+} catch (ExpiredException $e) {
+    http_response_code(401);
+    echo json_encode(["error" => "expired"]);
+} catch (Exception $e) {
+    http_response_code(401);
+    echo json_encode(["error" => "Invalid token"]);
+}
 ?>
